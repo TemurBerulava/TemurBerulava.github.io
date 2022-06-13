88d540eec3e6448b908099d58f0954043d95d6a9
TemurBerulava <temur.berulava@yandex.ru>
2022-06-14 00:14:51 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function clickHandler(){
+    const cont = document.getElementsByClassName('container1').item(0)
+    cont.classList.toggle('modify')
+    const nav = document.getElementsByClassName('nav').item(0)
+    nav.classList.toggle('change')
+}
+    
